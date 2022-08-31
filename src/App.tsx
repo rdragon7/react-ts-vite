be5554c49f4b0f9246ae703d5b700135f1b91742
coder7 <ren760824@163.com>
2022-08-31 18:29:06 +0800

    feat(less): less resubmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function App() {
-  return <div className="App">app...</div>
+  return (
+    <div className="App">
+      app...
+      <h2>哈啊哈</h2>
+      <a href="#">aaa</a>
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import './style/base.less'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
 )
