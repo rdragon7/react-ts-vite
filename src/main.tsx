cbdac028cb8994ff1ffd97407ba0c3cbc2d56315
coder7 <ren760824@163.com>
2022-08-31 18:42:27 +0800

    🎈 perf(alias): alias config

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import './style/base.less'
+import '@/style/base.less'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslintPlugin from 'vite-plugin-eslint'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()]
+  plugins: [react(), eslintPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
